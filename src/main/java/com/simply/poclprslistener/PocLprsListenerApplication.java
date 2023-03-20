4142d27c39295ebac23e59ba5e9e1697211e9346
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 10:02:06 +0800

    add component scan

--- src/main/java/com/simply/poclprslistener/PocLprsListenerApplication.java
+++ src/main/java/com/simply/poclprslistener/PocLprsListenerApplication.java
@@ -2,8 +2,10 @@ package com.simply.poclprslistener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = LprsController.class)
 public class PocLprsListenerApplication {
 
 	public static void main(String[] args) {
