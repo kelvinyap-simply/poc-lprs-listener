37aa51a280385a6c95bdb8f2ff6daab5e5d12b23
Kelvin Yap <kelvin.yap@simplysakal.asia>
2023-03-20 09:47:38 +0800

    Initial commit

---
+++ src/main/java/com/simply/poclprslistener/PocLprsListenerApplication.java
@@ -0,0 +1,13 @@
+package com.simply.poclprslistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocLprsListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocLprsListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simply/poclprslistener/PocLprsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.poclprslistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocLprsListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
